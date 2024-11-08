f546390b3ecf5e6b79dd83ab43f4363f5fc34740
DuckTieCorpMember <danyshbek1996@gmail.com>
2024-11-08 17:59:50 +0500

    Drawer stylization (WIP)

---
+++ wwwroot/js/auth.js
@@ -0,0 +1,6 @@
+﻿const user = {
+    email: 'jheart@dx-email.com',
+    name: 'John',
+    lastName: 'Heart',
+    avatarUrl: 'https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/employees/01.png',
+};
