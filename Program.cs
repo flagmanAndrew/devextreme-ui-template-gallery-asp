09a8dafe00bcd2f50090e59b591b4f4cc2a42a1f
Tom <artem.kurchenko@devexpress.com>
2025-01-20 14:36:59 +0400

    Build TS and SCSS

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var viewBuilder = builder.Services.AddControllersWithViews();
-
+#if DEBUG
+builder.Services.AddSassCompiler();
+#endif
 viewBuilder.AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 if (builder.Environment.IsDevelopment()) {
